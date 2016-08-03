917e2ecbc71352fb4cbe782278ed254b1d946338
nayeemjoy <porajitojoy@gmail.com>
2016-08-03 22:18:53 +0600

    Git Ignore Added

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import {Component} from 'angular2/core';
+
+@Component({
+	selector : 'pm-app',
+	template :`
+	<div><h1>{{pageTitle}}</h1>
+		<div>Hello World</div>
+	</div>
+	`
+})
+
+export class AppComponent{
+	pageTitle : string = "Welcome Angular 2"
+}
